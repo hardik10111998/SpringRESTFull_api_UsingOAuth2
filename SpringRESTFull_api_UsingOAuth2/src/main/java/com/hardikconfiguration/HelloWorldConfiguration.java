92a625b97fb33dd854073cd1dfb2dad58fa2604e
HARDIK <HARDIK@HARDIK>
2020-05-05 10:32:07 +0530

    First Commit

---
+++ SpringRESTFull_api_UsingOAuth2/src/main/java/com/hardikconfiguration/HelloWorldConfiguration.java
@@ -0,0 +1,13 @@
+package com.hardikconfiguration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.hardik")
+public class HelloWorldConfiguration {
+	
+
+}
